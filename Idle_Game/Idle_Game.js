63de3a583f2a3f4fcd0246e782b2647e4e72523c
Zer0bix <106792642+Zer0bix@users.noreply.github.com>
2023-06-13 23:08:38 +1000

    Movement and update
     - Moved code for Idle_game into a folder titled Idle_Game
     - Added some code to the js file for switching between tabs.

---
+++ Idle_Game/Idle_Game.js
@@ -0,0 +1,38 @@
+var trees = 1;
+var tree = 1;
+var price = 1;
+var ex_tree = tree;
+var ex_price = price;
+function gain() {
+    click_tree()
+}
+window.onload = function() {
+    document.getElementById("output").innerHTML = trees;
+    document.getElementById("upgbut2").innerHTML = price;
+    setInterval(gain, 1000)
+
+}
+function click_tree() {
+    trees += tree
+    document.getElementById("output").innerHTML = trees;
+}
+function upgrade_click() {
+    if(trees>price-1) {
+        trees -= price
+        ex_tree = ex_tree+ex_price
+        ex_price = ex_price*1.2
+        tree = Math.round(ex_tree)
+        price = Math.round(ex_price)
+
+        document.getElementById("output").innerHTML = trees;
+        document.getElementById("upgbut2").innerHTML = price;
+        document.getElementById("click_power").innerHTML = tree;
+    }
+}
+document.getElementById("upgbut").innerHTML = price;
+
+function open_tab(tab) {
+    if (document.getElementById("Games_Table").style.display === "none")
+        document.getElementById("Games_Table").style.display = "block";
+    else document.getElementById("Games_Table").style.display = "none";
+}
